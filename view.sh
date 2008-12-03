f9a33a5ff45769ffdf3bb919c52d2271ef444dc2
René Haber <rene@habr.de>
2008-12-03 13:09:45 +0100

    Enhanced view.sh to work with more than one file.

--- view.sh
+++ view.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
-
-gp_command="splot '< zcat $1' w d;pause -1;"
-echo "$gp_command" | gnuplot -persist -  
+args=("$@")
+command="splot "
+for (( i=0; i<${#args[@]}; i++ )); do
+	file="${args[${i}]}"
+	if ! [ -e "${file}" ]; then
+		echo "File ${file} not found."
+		exit
+	fi
+	dezip=""
+	if [[ "${file}" =~ ".gz" ]]; then
+		if which zcat &> /dev/null; then
+			dezip="< zcat "
+		fi
+		if which gzcat &> /dev/null; then
+			dezip="< gzcat "
+		fi
+	fi
+	command="${command} '${dezip}${file}' w d"
+	let last=$#-1
+	if [ $i -ne $last -a $# -gt 1 ]; then
+		command="${command}, "
+	fi
+	if [ $i -eq $last ]; then
+		command="${command}; pause -1"
+	fi
+done
+gnuplot -e "$command"
