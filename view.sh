22cba529cfde65f201cfe9574d5aa9fa635b115a
René Haber <rene@habr.de>
2013-12-09 16:48:54 +0100

    Updated view.sh

--- view.sh
+++ view.sh
@@ -1,23 +1,64 @@
-#!/bin/bash
+#!/bin/bash -e
 args=("$@")
 command="splot "
+last=`expr $# - 1`
+style="dots"
+
+function getMimeType {
+  echo $(file -b --mime-type $1)
+  return 0
+}
+
+function getNumCols {
+  dezip=""
+  file=""
+  if [ "$2" == "" ]
+  then
+    dezip="cat "
+    file=$1
+  else
+    dezip=${1:1}
+    file=$2
+  fi
+  echo $($dezip $file | grep -v '#' | awk '{print NF}' | sort -nu | tail -1)
+  return 0
+}
+
 for (( i=0; i<${#args[@]}; i++ )); do
-	file="${args[${i}]}"
-	if ! [ -e "${file}" ]; then
-		echo "File ${file} not found."
-		exit
-	fi
-	dezip=""
-	if [[ "${file}" =~ ".gz" ]]; then
-			dezip="<gzip -cd "
-	fi
-	command="${command} '${dezip}${file}' w d"
-	let last=$#-1
-	if [ $i -ne $last -a $# -gt 1 ]; then
-		command="${command}, "
-	fi
-	if [ $i -eq $last ]; then
-		command="${command}; pause -1"
-	fi
+  file="${args[${i}]}"
+  if ! [ -e "${file}" ]
+  then
+    echo "File ${file} not found."
+    exit
+  fi
+  dezip=""
+  mimeType=$(getMimeType $file)
+  case "$mimeType" in
+    "application/x-gzip" )
+      dezip="<gzip -cd "
+      ;;
+    "application/x-bzip2" )
+      dezip="<bzip2 -cd "
+      ;;
+  esac
+
+  if [ $i -eq 0 ]
+  then
+    numCols=$(getNumCols "$dezip" "$file")
+    if (( numCols < 3 ))
+    then
+      command="plot "
+      style="points"
+    fi
+  fi
+
+  command="${command} '${dezip}${file}' w $style t \"$file\""
+  if (( $i < $last && $# > 1 )); then
+    command="${command}, "
+  fi
+  if (( $i == $last )); then
+    command="${command}; pause -1"
+  fi
 done
+echo $command
 gnuplot -e "$command"
